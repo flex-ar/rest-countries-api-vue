a826877b96ce8e3f1288a94eb2d73b3e20b52180
flex-ar <jmarnedo8@gmail.com>
2023-03-18 17:43:25 -0300

    add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'warn',
+      {
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'es5',
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
